822c9e05e30270763b5048171d510ec1ad1ae877
liawzhengyi <lwzhengyi@gmail.com>
2024-08-13 01:00:25 +0800

    modified:   src/App.css 	modified:   src/components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src = {''} alt="Logo"></img>
+            <img src = {logo} alt="Logo"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -43,9 +43,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                {/* <a href="#"> <img src={} alt=""/> </a>
-                <a href="#"> <img src={} alt=""/> </a>
-                <a href="#"> <img src={} alt=""/> </a> */}
+                <a href="#"> <img src={navIcon1} alt=""/> </a>
+                <a href="#"> <img src={navIcon2} alt=""/> </a>
+                <a href="#"> <img src={navIcon3} alt=""/> </a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}> <span>Let's connect</span> </button>
           </span>
